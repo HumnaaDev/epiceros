14a9948af89149e264645d16d6331fec7f5a2381
Humna <humna.aleem111@gmail.com>
2023-09-21 15:32:04 +0500

    get teams data in sidebar comp

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,16 +1,16 @@
-import { useGetTeamData } from "@/services/registration"
+import { useGetTeamData } from "@/services/register"
 import { SidebarWrapper } from "./sidebar.style"
 import { Typography } from "@mui/material"
 
 export const Sidebar = () => {
-    const { data: team } = useGetTeamData()
+    const { data } = useGetTeamData()
 
     return (
         <SidebarWrapper className="left-area">
             <Typography className="text-white" variant="h2" component="h2">Join the team</Typography>
             <ul className="list text-white">
                 {
-                    team?.map((name) => (<li>{name}</li>))
+                    data?.team?.map((name: string, index: number) => (<li key={`${name}-${index}`}>{name}</li>))
                 }
             </ul>
         </SidebarWrapper>
